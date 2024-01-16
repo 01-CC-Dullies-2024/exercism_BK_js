4c5eed3e86a7d54626d2f1b9a7ca1921f1867069
BKUE136 <155626099+BKUE136@users.noreply.github.com>
2024-01-16 10:05:38 +0000

    added env++

---
+++ src/01_hello_world.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+const HW_string = "Hello, World!"
+
+export function hello() {
+  return HW_string;
+}
